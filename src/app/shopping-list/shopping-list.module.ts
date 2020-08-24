badbf77ac7c1e1ceddde2790b5fbeec16edc5d4e
Bhairesh <bhairesh@arithaconsulting.com>
2020-08-24 20:08:56 +0530

    fix

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -10,7 +10,6 @@ import { SharedModule } from "../shared/shared.module";
   imports: [
     SharedModule,
     FormsModule,
-    RouterModule,
     RouterModule.forChild([
       {
         path: "shopping-list",
